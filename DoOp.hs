f811e6395a3a6793ba98df8cf1990a76d1f2a3f8
Clément <clement2104.boillot@gmail.com>
2024-01-04 09:21:37 +0100

    Add 2nd task

--- DoOp.hs
+++ DoOp.hs
@@ -9,3 +9,7 @@ myElem :: Eq a => a -> [a] -> Bool
 myElem _ [] = False
 myElem comp (e:arr) | comp == e = True
                     | otherwise = myElem comp arr
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv _ 0 = Nothing
+safeDiv x y = Just $ div x y
